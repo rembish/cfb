40af7bbb6bbdce9bd95a44a8076ccd991d477a89
Alex Rembish <aleksey.rembish@firma.seznam.cz>
2013-09-24 08:37:59 +0200

    Core implementing

---
+++ cfb/__init__.py
@@ -0,0 +1,128 @@
+from io import FileIO, StringIO
+from struct import unpack
+
+
+
+class CfbError(Exception):
+    pass
+
+
+class CfbDefect(CfbError):
+    pass
+
+
+class FatalDefect(CfbDefect):
+    pass
+
+
+class ErrorDefect(CfbDefect):
+    pass
+
+
+class WarningDefect(CfbDefect):
+    pass
+
+
+class ClsId(object):
+    def __init__(self, value):
+        pass
+
+CLSID_NULL = ClsId(0)
+
+
+class Defected(object):
+    def raise_if(self, exception, *args, **kwargs):
+        raise exception(*args, **kwargs)
+
+    def _fatal(self, *args, **kwargs):
+        return self.raise_if(FatalDefect, *args, **kwargs)
+
+    def _error(self, *args, **kwargs):
+        return self.raise_if(ErrorDefect, *args, **kwargs)
+
+    def _warning(self, *args, **kwargs):
+        return self.raise_if(WarningDefect, *args, **kwargs)
+
+
+class Header(StringIO, Defected):
+    signature = 0xD0CF11E0A1B11AE1
+
+    def __init__(self, data):
+        super(Header, self).__init__(data)
+
+        if unpack('>Q', self.read(8))[0] != self.signature:
+            self._fatal('Identification signature for the compound file '
+                        'structure, and MUST be set to the value 0xD0, '
+                        '0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1.')
+
+        self.clsid = ClsId(self.read(16))
+        if self.clsid != CLSID_NULL:
+            self._error('Reserved and unused class ID that MUST be set to all '
+                        'zeroes (CLSID_NULL).')
+
+        minor, major, byte_order, sector_shift, mini_sector_shift = \
+            unpack('<HHHHH', self.read(10))
+
+        if major not in (3, 4):
+            self._error('Version number for breaking changes. This field MUST '
+                        'be set to either 0x0003 (version 3) or 0x0004 '
+                        '(version 4).')
+        if minor != 0x003E:
+            self._warning('Version number for non-breaking changes. This '
+                          'field SHOULD be set to 0x003E if the major version '
+                          'field is either 0x0003 or 0x0004.')
+
+        if byte_order != 0xFFFE:
+            self._fatal('This field MUST be set to 0xFFFE. This field is a '
+                        'byte order mark for all integer fields, specifying '
+                        'little-endian byte order.')
+
+        if sector_shift not in (0x0009, 0x000C):
+            self._error('This field MUST be set to 0x0009, or 0x000c, '
+                        'depending on the Major Version field.')
+        if sector_shift == 0x0009 and major != 3:
+            self._error('If Major Version is 3, then the Sector Shift MUST be '
+                        '0x0009, specifying a sector size of 512 bytes.')
+        if sector_shift == 0x000C and major != 4:
+            self._error('If Major Version is 4, then the Sector Shift MUST be '
+                        '0x000C, specifying a sector size of 4096 bytes.')
+
+        if mini_sector_shift != 0x0006:
+            self._error('This field MUST be set to 0x0006. This field '
+                        'specifies the sector size of the Mini Stream as '
+                        'a power of 2.')
+
+        if self.read(6) != '\0' * 6:
+            self._error('Reversed field MUST be set to all zeroes.')
+
+        (self.number_of_directory_sectors, self.number_of_fat_sectors,
+         self.first_directory_sector_location,
+         self.transaction_signature_number,
+         self.mini_stream_cutoff_size, self.first_mini_fat_sector_location,
+         self.number_of_mini_fat_sectors, self.first_difat_sector_location,
+         self.number_of_difat_sectors) = unpack('<LLLLLLLLL', self.read(36))
+
+        if major == 3 and self.number_of_directory_sectors:
+            self._error('If Major Version is 3, then the Number of Directory '
+                        'Sectors MUST be zero. This field is not supported '
+                        'for version 3 compound files.')
+
+        if self.mini_stream_cutoff_size != 0x00001000:
+            self._error('This integer field MUST be set to 0x00001000. This '
+                        'field specifies the maximum size of a user-defined '
+                        'data stream allocated from the mini FAT and mini '
+                        'stream, and that cutoff is 4096 bytes. Any '
+                        'user-defined data stream larger than or equal to '
+                        'this cutoff size must be allocated as normal sectors '
+                        'from the FAT.')
+
+        self.version = major, minor
+        self.sector_size = 2 ** sector_shift
+        self.mini_sector_size = 2 ** mini_sector_shift
+
+
+
+class CfbIO(FileIO, Defected):
+    def __init__(self, name, mode='r'):
+        super(CfbIO, self).__init__(name, mode=mode)
+        self.header = Header(self.read(512))
